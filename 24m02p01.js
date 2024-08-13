6edab1581018d488af69d63018a69b43c3c9a349
OchaOIQ <122747842+OchaOIQ@users.noreply.github.com>
2024-08-13 17:58:18 +0300

    Update 24m02p01.js
    обновление алгоритма нахождения рекламы под новые версии яндекса

--- 24m02p01.js
+++ 24m02p01.js
@@ -7,6 +7,7 @@
 // @license      MIT
 // @author       Mysty
 // @match        https://yandex.ru/*
+// @match        https://ya.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yandex.ru
 // @grant        none
 // ==/UserScript==
@@ -14,17 +15,25 @@
 (function () {
     'use strict';
     var spans = document.getElementsByTagName("span");
+    var classElements = document.getElementsByClassName("KZJmITygmS");
+
+    function highlightAd(element) {
+        if (element.innerText.trim() === "Реклама") {
+            element.style.fontWeight = 'bold';
+            element.style.color = 'red';
+            element.style.textAlign = 'right';
+            var layer1 = element.parentElement;
+            var layer2 = layer1.parentElement;
+            var container = layer2.parentElement;
+            container.style.border = '2pt groove red';
+        }
+    }
 
     for (var i = 0; i < spans.length; i++) {
-    if (spans[i].innerText.trim() === "Реклама") {
-        spans[i].style.fontWeight = 'bold';
-        var fontSize = parseInt(window.getComputedStyle(spans[i], null).getPropertyValue('font-size'));
-        spans[i].style.color = 'red';
-        spans[i].style.textAlign = 'right';
-        var layer1 = spans[i].parentElement;
-        var layer2 = layer1.parentElement;
-        var container = layer2.parentElement;
-        container.style.border = '2pt groove red';
-        }
+        highlightAd(spans[i]);
+    }
+
+    for (var j = 0; j < classElements.length; j++) {
+        highlightAd(classElements[j]);
     }
 })();
